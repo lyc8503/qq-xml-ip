3f03510305b9b2ba5e115eae0dc0af89f312da02
lyc8503 <1553308251@qq.com>
2020-08-06 14:24:58 +0800

    update.

--- template.php
+++ template.php
@@ -1,7 +1,7 @@
 <head>
 	<meta itemprop="name" content="<?php echo $_GET["xmltitle"]; ?>" />
 	<meta itemprop="description" content="<?php echo $_GET["description"]; ?>" />
-	<meta itemprop="image" content="<?php echo $_GET["domain"]; ?>ip.php?id=<?php echo $_GET["id"]; ?>" />   //此处注意链接带上参数id
+	<meta itemprop="image" content="<?php echo $_GET["domain"]; ?>ip.php?id=<?php echo $_GET["id"]; ?>" />   
 	<title>QQ Xml 模板</title>
 </head>
 <body>生成成功.</body>
