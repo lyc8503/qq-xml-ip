3bd0d3f2abc0c0c4b184da7709ce8533e0c3630f
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2020-08-06 14:00:04 +0800

    Update template.php

--- template.php
+++ template.php
@@ -1,7 +1,7 @@
 <head>
-	<meta itemprop="name" content="xml卡片标题" />
-	<meta itemprop="description" content="xml卡片描述" />
-	<meta itemprop="image" content="iptest.php文件链接?id=<?php echo $_GET["id"]; ?>" />   //此处注意链接带上参数id
+	<meta itemprop="name" content="<?php echo $_GET["xmltitle"]; ?>" />
+	<meta itemprop="description" content="<?php echo $_GET["description"]; ?>" />
+	<meta itemprop="image" content="<?php echo $_GET["domain"]; ?>ip.php?id=<?php echo $_GET["id"]; ?>" />   //此处注意链接带上参数id
 	<title>QQ Xml 模板</title>
 </head>
 <body>生成成功.</body>
