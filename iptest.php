1c6525743654abced5b219a6321d941915e4b52e
lyc8503 <36782264+lyc8503@users.noreply.github.com>
2020-08-06 13:37:32 +0800

    Update iptest.php

--- iptest.php
+++ iptest.php
@@ -2,5 +2,5 @@
 	<meta itemprop="name" content="xml卡片标题" />
 	<meta itemprop="description" content="xml卡片描述" />
 	<meta itemprop="image" content="iptest.php文件链接?id=<?php echo $_GET["id"]; ?>" />   //此处注意链接带上参数id
-	<title>标题</title>
+	<title>QQ Xml 模板</title>
 </head>
